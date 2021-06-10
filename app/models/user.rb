1a6b8ca0d583089a8d83dd3c403db5db9d76eee8
hukai <huzhengen@163.com>
2021-06-10 12:34:13 +0800

    I18n validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :email
   validates_presence_of :password_confirmation, on: [:create]
-  validates_format_of :email, with: /.*@.*/
-  validates :password, length: { minimum: 6 }, on: [:create]
+  validates_format_of :email, with: /.*@.*/, if: :email
+  validates :password, length: { minimum: 6 }, on: [:create], if: :password
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,5 @@
+# 应用可用的区域设置白名单
+I18n.available_locales = [:en, "zh-CN"]
+
+# 修改默认区域设置（默认是 :en）
+I18n.default_locale = "zh-CN"
