21a4d147ee351bbda5dea90c25bd4298d1fb6bf2
hukai <huzhengen@163.com>
2021-06-10 12:07:55 +0800

    remove password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates_presence_of :email
-  validates_presence_of :password, :password_confirmation, on: [:create]
+  validates_presence_of :password_confirmation, on: [:create]
 end
